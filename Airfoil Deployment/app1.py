5214a825e984bed054e90b05ff35fa2ee6482e14
shinigamiloveapple <62288252+shinigamiloveapple@users.noreply.github.com>
2020-06-11 01:20:00 +0530

    Update app1.py

--- Airfoil Deployment/app1.py
+++ Airfoil Deployment/app1.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('airfoil_best_model.pkl', 'rb'))
+model = pickle.load(open('airfoil_RFR_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
